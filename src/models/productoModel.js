6bd35a0b4cd2ab7c28f4e3a91afc4319b36c49ee
ERScara <esteban.scaramuzza@gmail.com>
2024-02-06 19:06:12 -0300

    Add files via upload

---
+++ src/models/productoModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb://mongodb:27017/empresa", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+
+});
+const ProductoSchema = new mongoose.Schema(
+  {     
+    nombre: String,
+    precio: String,
+  },
+  { collection: "productos" }
+);
+
+const Producto = mongoose.model("Producto", ProductoSchema);
+
+module.exports = Producto;
