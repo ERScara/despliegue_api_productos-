a93e7618b1867af4d67132c3315046035b5ffd33
ERScara <esteban.scaramuzza@gmail.com>
2024-02-17 01:47:52 -0300

    Add files via upload

---
+++ coverage/src/models/productoModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const ProductoSchema = new mongoose.Schema(
+  {
+    nombre: String,
+    precio: String,
+  },
+  { collection: "productos" }
+);
+
+const Producto = mongoose.model("Producto", ProductoSchema);
+
+module.exports = Producto;
